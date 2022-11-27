7a05f4a59b69c9360d0648d0b1c35ac2fdb873ac
Mohammad Robati Shirzad <mrobatishirzad@uwaterloo.ca>
2022-11-26 19:41:55 -0500

    bug fix added

--- implementation/1-mining/embedding/RQ1/rq1_ex_gen.py
+++ implementation/1-mining/embedding/RQ1/rq1_ex_gen.py
@@ -22,7 +22,7 @@ c = 1
 for line in samples_file:
     parts = line.split(" ")
     try:
-        src = "./visualization/"+parts[0].replace("/", "_")+"/"+parts[1]+"/"+parts[2]+"_"+parts[3].replace("\n", "")+".png"
+        src = "./circlepacks/"+parts[0].replace("/", "_")+"/"+parts[1]+"/"+parts[2]+"_"+parts[3].replace("\n", "")+".png"
         open(src, "r")
         html_file.write("<br><br>")
         html_file.write("DP#"+str(c)+": <a target=\"_blank\" href=\""+"https://github.com/"+parts[0]+"/commit/"+parts[1]+"\">Link</a>\n")
